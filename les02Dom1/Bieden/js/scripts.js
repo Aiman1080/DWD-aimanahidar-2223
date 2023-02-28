039e960f3aecf591dcf9a0d4d3cd6803657fd05e
Aiman1080 <114600986+Aiman1080@users.noreply.github.com>
2023-02-28 17:17:20 +0100

    commit les 3

---
+++ les02Dom1/Bieden/js/scripts.js
@@ -0,0 +1,26 @@
+const msg = document.querySelector('#message');
+const btn = document.querySelector('#button');
+const inpNaam = document.querySelector('#naam');
+const inpBod = document.querySelector('#prijs');
+
+let hoogsteBod = 0;
+let helpNaam;
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (inpNaam.value !== '' && inpBod.value !== '') {
+    const helpBod = parseInt(inpBod.value);
+    if (helpBod > hoogsteBod) { 
+    hoogsteBod = helpBod;
+    helpNaam = inpNaam.value;
+    msg.innerHTML = 'gefeliciteerd! je hebt momenteel het \n hoogste bod';
+    } else {
+    msg.innerHTML = `jammer! ${helpNaam} heeft een hoger bod`;
+    }
+} else {
+    msg.innerHTML = 'er is nog geen bod uitgebracht';
+}
+    inpNaam.value = '';
+    inpBod.value = '';
+});
---
+++ les02Dom1/TaskList/js/scripts.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('#frmTask');
+const taak = document.querySelector('#txtTask');
+const datum = document.querySelector('#datDeadline');
+const lijst = document.querySelector('#tasks');
+
+const dropBox = document.querySelector('#selPriority');
+
+btn.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let kleur;
+    const prioriteit = dropBox[dropBox.selectedIndex].value;
+
+    if (prioriteit == 'low') {
+        kleur = 'green';
+    } else if (prioriteit == 'normal') {
+        kleur = 'orange';
+    } else if (prioriteit == 'high') {
+        kleur = 'red';
+    }
+
+    const hulpHtml = `<div class="task"><span class="priority ${kleur} material-icons">assignment</span><p class="tasktext">${taak.value} <span class="deadline">(deadline: ${datum.value})</span></p><span class="complete material-icons">more_horiz</span></div>`;
+    lijst.innerHTML += hulpHtml;
+    taak.value = '';
+    datum.value = '';
+});
+lijst.addEventListener('click',function (e) {
+    if (e.target.innerHTML == 'more_horiz') {
+        e.target.innerHTML = 'done';
+        e.target.classList.add('done');
+    }
+});
