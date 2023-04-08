bdf436032f67d8c9783bcf2592566ceebe8d34b5
Aiman1080 <114600986+Aiman1080@users.noreply.github.com>
2023-04-08 03:20:51 +0200

    commit project

--- project/js/script.js
+++ project/js/script.js
@@ -1,24 +1,25 @@
 const form = document.querySelector('form');
 const buttonsDiv = document.querySelector('#buttons');
+const key = 'Wzy8gud0klcnIVIqLTsiOZDsnaAlyJEsZHQ0Z8Mo';
 
-async function someFetchFunction(e) {
-    let url = `https://freesound.org/apiv2/search/text/?query=${e}`;
+async function someFetchFunction(query) {
+  let url = `https://freesound.org/apiv2/search/text/?query=${query}&key=${key}`;
 
-    // fetch
-    const resp = await fetch(url);
-    if (!resp.ok) {
-        console.log('Erreur de fetch');
-        return;
-    }
+  // fetch
+  const resp = await fetch(url);
+  if (!resp.ok) {
+    console.log('Erreur de fetch');
+    return;
+  }
 
-    // get json data
-    const data = await resp.json();
+  // get json data
+  const data = await resp.json();
 
-    console.log(data);
+  console.log(data);
 }
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
-  const zoek = document.querySelector('#zoek').value;
-  someFetchFunction(zoek);
+  const query = document.querySelector('#zoek').value;
+  await someFetchFunction(query);
 });
