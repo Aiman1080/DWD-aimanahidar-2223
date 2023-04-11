c4a68040813dc1c4a6b5cab4ad16dc96241dbd62
Aiman1080 <114600986+Aiman1080@users.noreply.github.com>
2023-04-11 08:22:26 +0200

    commit project

--- project/js/script.js
+++ project/js/script.js
@@ -3,7 +3,10 @@ const buttonsDiv = document.querySelector('#buttons');
 const key = 'Wzy8gud0klcnIVIqLTsiOZDsnaAlyJEsZHQ0Z8Mo';
 const btnZoeken = document.querySelector('form button');
 const favBtn = document.querySelector('#favBtn');
-const audioBtns = document.querySelectorAll('.audioBtn');
+const favoritesDiv = document.querySelector('#favDiv');
+const favContainer = document.querySelector('#fav');
+
+favContainer.classList.add('hidden');
 
 const url = `https://freesound.org/apiv2/search/text/?token=${key}`;
 
@@ -50,14 +53,34 @@ function create(results) {
 
     const audioBtns = document.querySelectorAll('.audioBtn');
     audioBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            const audio = btn.nextElementSibling;
-            audio.play();
+        btn.addEventListener('click', function () {
+            const audio = this.nextElementSibling;
+            if (audio.paused) {
+                audio.play();
+                this.classList.add('playing');
+            } else {
+                audio.pause();
+                audio.currentTime = 0;
+                this.classList.remove('playing');
+            }
         });
     });
 }
 
-const favoritesDiv = document.querySelector('#favDiv');
+const buttons = document.querySelectorAll('.audioBtn');
+const audios = document.querySelectorAll('.audio');
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener('click', function() {
+    audios.forEach((audio, audioIndex) => {
+      if (audioIndex !== index) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    });
+    audios[index].play();
+  });
+});
 
 function addToFavorites(button) {
     const soundCard = button.parentNode;
@@ -65,24 +88,56 @@ function addToFavorites(button) {
     const deleteButton = soundCardClone.querySelector('.delete-btn');
     deleteButton.removeAttribute('disabled');
     favoritesDiv.appendChild(soundCardClone);
+    favContainer.classList.remove('hidden');
+
+    const favoriteSounds = JSON.parse(localStorage.getItem('favoriteSounds')) || [];
+    const sound = { id: soundCardClone.id, html: soundCardClone.outerHTML };
+    favoriteSounds.push(sound);
+    localStorage.setItem('favoriteSounds', JSON.stringify(favoriteSounds));
 }
 
 function deleteFromFavorites(button) {
     const soundCard = button.parentNode;
     soundCard.remove();
+
+    const favoritesDiv = document.querySelector('#favDiv');
+    const favoritesList = favoritesDiv.querySelectorAll('.sound-card');
+    if (favoritesList.length === 0) {
+        const favoritesHeader = document.querySelector('#fav p');
+        removeLocalStorage(favoritesHeader.dataset.id);
+        const favoritesSection = document.querySelector('#fav');
+        favoritesSection.style.display = 'none';
+    }
+
+    const favoriteSounds = JSON.parse(localStorage.getItem('favoriteSounds')) || [];
+    const update = favoriteSounds.filter((sound) => sound.id !== soundCard.id);
+    localStorage.setItem('favoriteSounds', JSON.stringify(update));
 }
 
 function sound() {
     const audio = this.closest('.audio').querySelector('audio');
+    const activeBtn = document.querySelector('.audioBtn.active');
+    if (activeBtn && activeBtn !== this) {
+        const activeAudio = activeBtn.nextElementSibling;
+        activeAudio.pause();
+        activeBtn.classList.remove('active');
+    }
     if (!audio.paused) {
         audio.pause();
         this.classList.remove('active');
     } else {
+        const allAudios = document.querySelectorAll('.audioBtn');
+        allAudios.forEach(btn => {
+            const otherAudio = btn.nextElementSibling;
+            if (otherAudio !== audio) {
+                otherAudio.pause();
+                btn.classList.remove('active');
+            }
+        });
         audio.play();
         this.classList.add('active');
     }
 }
-
 function setRandomColor(element) {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
@@ -90,8 +145,16 @@ function setRandomColor(element) {
     element.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     return element;
 }
+
+const audioBtns = document.querySelectorAll('.audioBtn');
 audioBtns.forEach(btn => {
     btn = setRandomColor(btn);
     btn.addEventListener('click', sound);
 });
+
+function removeLocalStorage(x) {
+    const save = JSON.parse(localStorage.getItem('favorite')) || [];
+    const update = save.filter((sound) => sound.id !== x);
+    localStorage.setItem('favorite', JSON.stringify(update));
+}
 
